f80edc7edf9375b7308d726a3e9733b068ab0fb8
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2023-10-26 12:29:41 +0300

    init the defult classes

---
+++ src/main/java/com/mycompany/terminal/Terminal.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.terminal;
+
+/**
+ *
+ * @author Abelrhman Mostafa
+ * @author Ahmed Hanfy
+ */
+public class Terminal {
+    Parser parser;
+    //Implement each command in a method, for example:
+    public String pwd(){
+        return "";
+    }
+    public void cd(String[] args){
+
+    }
+    // ...
+    //This method will choose the suitable command method to be called
+    public void chooseCommandAction(){
+
+    }
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+
+}
+
