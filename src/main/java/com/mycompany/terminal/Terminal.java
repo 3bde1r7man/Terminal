76832591a8447c5915b57e4408077881e97aa09b
Ahmed Hanfy <50291321+ahanfybekheet@users.noreply.github.com>
2023-10-27 00:16:33 +0000

    add remaining commands

--- src/main/java/com/mycompany/terminal/Terminal.java
+++ src/main/java/com/mycompany/terminal/Terminal.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList; 
 
 /**
  *
@@ -18,50 +19,53 @@ import java.util.Scanner;
 
 public class Terminal {
     Parser parser;
+    ArrayList<String> history = new ArrayList<String>();
+    ArrayList<String> output = new ArrayList<String>();
+    ArrayList<String> error = new ArrayList<String>();
 
     // echo takes 1 arg and print it
     public void echo(String[] args){
         if (args.length >= 1) {
             String message = String.join(" ", args);
-            System.out.println(message);
+            output.add(message);
         } else {
-            System.out.println("No message to echo.");
+            error.add("No message to echo.");
         }
     }
 
     // print the current working directory
     public void pwd(){
-        System.out.println(System.getProperty("user.dir"));
+        output.add(System.getProperty("user.dir"));
     }
 
     // Change the current working directory to a given path 
     public void cd(String[] args){
         // If no passed arguments
         if(args.length == 0){
-            System.out.println("You must specify a directory.");
+            output.add("You must specify a directory.");
         }else if(args.length == 1 && "..".equals(args[0])){
             File currentDirectory = new File(System.getProperty("user.dir"));
             // Navigate up one directory level
             File parentDirectory = currentDirectory.getParentFile();
             System.setProperty("user.dir", parentDirectory.getAbsolutePath());
-            System.out.println("Changed working directory to: " + System.getProperty("user.dir"));
+            output.add("Changed working directory to: " + System.getProperty("user.dir"));
         }else if (args.length == 1) {
             File directory = new File(args[0]);
             if (directory.exists() && directory.isDirectory()) {
                 // The path exists and is a directory
-                System.out.println("The directory exists: " + directory.getAbsolutePath());
+                error.add("The directory exists: " + directory.getAbsolutePath());
 
                 // Change the current working directory
                 if (directory.isDirectory()) {
                     System.setProperty("user.dir", directory.getAbsolutePath());
-                    System.out.println("Changed working directory to: " + System.getProperty("user.dir"));
+                    output.add("Changed working directory to: " + System.getProperty("user.dir"));
                 }
             } else {
                 // The path does not exist or is not a directory
-                System.out.println("Invalid path or The directory does not exist: " + directory.getAbsolutePath());
+                error.add("Invalid path or The directory does not exist: " + directory.getAbsolutePath());
             }
         }else { // If more than one argument
-            System.out.println("There is no specified path.");
+            output.add("There is no specified path.");
         }
     }
     
@@ -73,25 +77,25 @@ public class Terminal {
         File[] files = currentDirectory.listFiles();
         
         if (files != null) {
-            System.out.println("Files in the current directory:");
+            output.add("Files in the current directory:");
             if(args.length == 1 && "-r".equals(args[0])) {
                 for (int i = files.length - 1; i >= 0; i--) {
-                    System.out.println(files[i].getName());
+                    output.add(files[i].getName());
                 }
             } else if(args.length == 0){
                 for (File file : files) {
-                    System.out.println(file.getName());
+                    output.add(file.getName());
                 }
             }
             else {
-                System.out.println("Unknown argument.");
+                error.add("Unknown argument.");
             }
         }
     }
     // 
     public void mkdir(String[] args){
         if(args.length == 0){
-            System.out.println("No arguments specified.");
+            error.add("No arguments specified.");
         }
         String path = args[0];
         if(args.length > 1){
@@ -99,12 +103,12 @@ public class Terminal {
         }
         File directory = new File(path);
         if (directory.exists()) {
-            System.out.println("Directory '" + path + "' already exists.");
+            error.add("Directory '" + path + "' already exists.");
         } else {
             if (directory.mkdirs()) {
-                System.out.println("Directory '" + path + "' created successfully.");
+                output.add("Directory '" + path + "' created successfully.");
             } else {
-                System.err.println("Failed to create directory '" + path + "'.");
+                error.add("Failed to create directory '" + path + "'.");
             }
         }
     }
@@ -114,11 +118,11 @@ public class Terminal {
             String path = args[0];
             if(args.length > 1){
                 path = String.join(" ", args);
-                System.out.println(path);
+                output.add(path);
             }
             File file = new File(path);
             file.createNewFile();
-            System.out.println("File " + file.getName() + " created successfully.");
+            output.add("File " + file.getName() + " created successfully.");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -126,17 +130,17 @@ public class Terminal {
     // rmoves file from the current directory
     public void rm(String[] args){
         if(args.length == 0){
-            System.out.println("You must specify a file name.");
+            error.add("You must specify a file name.");
         }else if(args.length == 1){
             File file = new File(args[0]);
             if (file.exists()) {
                 if (file.delete()) {
-                    System.out.println("File " + args[0] + " has been removed.");
+                    output.add("File " + args[0] + " has been removed.");
                 } else {
-                    System.out.println("Unable to remove the file " + args[0]);
+                    output.add("Unable to remove the file " + args[0]);
                 }
             } else {
-                System.out.println("File " + args[0] + " does not exist in the current directory.");
+                error.add("File " + args[0] + " does not exist in the current directory.");
             }
         }
         
@@ -148,7 +152,7 @@ public class Terminal {
         }else if (args.length == 2){
             concatenateAndPrintFiles(args[0], args[1]);
         }else{
-            System.out.println("no arguments specified or more arguments specified.");
+            error.add("no arguments specified or more arguments specified.");
         }
     }
     public void printFileContent(String fileName) {
@@ -156,11 +160,11 @@ public class Terminal {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
             String line;
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+                output.add(line);
             }
             reader.close();
         } catch (IOException e) {
-            System.err.println("Error reading the file: " + e.getMessage());
+            error.add("Error reading the file: " + e.getMessage());
         }
     }
     
@@ -171,12 +175,12 @@ public class Terminal {
 
             String line;
             while ((line = reader1.readLine()) != null) {
-                System.out.println(line);
+                output.add(line);
             }
             reader1.close();
 
             while ((line = reader2.readLine()) != null) {
-                System.out.println(line);
+                output.add(line);
             }
             reader2.close();
         } catch (IOException e) {
@@ -204,14 +208,63 @@ public class Terminal {
             System.err.println("Error reading the file: " + e.getMessage());
         }
 
-        System.out.println("Lines: " + lineCount);
-        System.out.println("Words: " + wordCount);
-        System.out.println("Characters: " + charCount);
-        System.out.println("File Name: " + fileName);
+        output.add("Lines: " + lineCount);
+        output.add("Words: " + wordCount);
+        output.add("Characters: " + charCount);
+        output.add("File Name: " + fileName);
+    }
+
+    // history command
+    public void history(String[] args){
+        if(args.length == 0){
+            for(int i = 0; i < history.size(); i++){
+                output.add(history.get(i));
+            }
+        }else{
+            error.add("Unknown argument.");
+        }
+    }
+
+    // rmdir command delete directory only if it is empty
+    public void rmdir(String[] args){
+        if(args.length == 0){
+            error.add("You must specify a directory name.");
+        }else if(args.length == 1){
+            if(args[0].equals("*")){
+                File currentDirectory = new File(System.getProperty("user.dir"));
+                File[] files = currentDirectory.listFiles();
+                for (File file : files) {
+                    if(file.isDirectory()){
+                        File[] subDirs = file.listFiles();
+                        if(subDirs.length == 0){
+                            file.delete();
+                        }
+                    }
+                }
+            }else{
+                File file = new File(args[0]);
+                if (file.exists()) {
+                    File[] subDirs = file.listFiles();
+                    if(subDirs.length == 0){
+                        if (file.delete()) {
+                            output.add("Directory " + args[0] + " has been removed.");
+                        } else {
+                            error.add("Unable to remove the directory " + args[0]);
+                        }
+                    }else{
+                        error.add("Directory " + args[0] + " is not empty.");
+                    }
+                } else {
+                    error.add("Directory " + args[0] + " does not exist in the current directory.");
+                }
+            }
+            
+        }
     }
 
     //This method will choose the suitable command method to be called
     public void chooseCommandAction(){
+        history.add(parser.getCommandName());
         if (parser.getCommandName().equals("echo")) {
             echo(parser.getArgs());
         } else if (parser.getCommandName().equals("pwd")) {
@@ -230,10 +283,12 @@ public class Terminal {
             cat(parser.getArgs());
         } else if(parser.getCommandName().equals("wc")) {
             wc(parser.getArgs());
+        } else if (parser.getCommandName().equals("history")) {
+            history(parser.getArgs());
         } else if (parser.getCommandName().equals("exit")) {
             System.exit(0);
         } else {
-            System.out.println("Unknown command: " + parser.getCommandName());
+            error.add("Unknown command: " + parser.getCommandName());
         }
     }
     public static void main(String[] args){
@@ -250,7 +305,7 @@ public class Terminal {
             System.out.println("Failed to parse the command.");
         }
         scanner.close();
-        
+
     }
 
 }
